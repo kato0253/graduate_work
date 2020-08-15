16ac6ca0de00555b186647fa7a30b2a7da08789a
kato <user@MacBook-Air.local>
2020-08-15 14:18:37 +0900

    add:コメント機能にVue.js追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_blog, only: [:create, :edit, :update]
   # コメントを保存、投稿するためのアクションです。
   def create
     # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
@@ -15,9 +16,31 @@ class CommentsController < ApplicationController
       end
     end
   end
+  def edit
+    @comment = @article.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+  def update
+    @comment = @article.comments.find(params[:id])
+    respond_to do |format|
+      if @comment.update(comment_params)
+        flash.now[:notice] = 'コメントが編集されました'
+        format.js { render :index }
+      else
+        flash.now[:notice] = 'コメントの編集に失敗しました'
+        format.js { render :edit_error }
+      end
+    end
+  end
   private
   # ストロングパラメーター
   def comment_params
     params.require(:comment).permit(:article_id, :content)
+  end
+  def set_blog
+    @article = Article.find(params[:article_id])
   end
 end
