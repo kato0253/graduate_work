3e2d0d613eefb9d1cedb1de2d5556edbc20c27a5
kato <user@MacBook-Air.local>
2020-08-19 16:41:42 +0900

    add:seedデータ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
-  
+
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
