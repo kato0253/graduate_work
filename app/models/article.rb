2ecbdf0ddecf71d0330f472288d3df8747e40eb7
kato <user@MacBook-Air.local>
2020-08-19 11:15:08 +0900

    add:バリデーション追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
